d0bb853f4869e2966047deedada2704c04185f4a
fernando <fernando@fernando-Satellite-P855>
2015-07-10 22:32:19 -0500

    Version->primera

---
+++ src/java/Util/MyUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+/**
+ *
+ * @author fernando
+ */
+public class MyUtil {
+    public  static String baseurl(){
+        return "http://localhost:8080/ProcesosProyectoFinalTesis/";
+    }
+    public  static String basepathLOgin(){
+    return "/ProcesosProyectoFinalTesis/faces/";
+    }
+    public static String basepath(){
+    return "/faces/views/";
+    }
+    
+}
